cfb4fb5b7c7d37b98f41edf366238df148ac74aa
rakib8680 <rakib.cst.1st@gmail.com>
2023-03-21 19:08:11 +0600

    data dynamically added

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -2,9 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    { name: 'Samsung', price: 30000 },
+    { name: 'Apple', price: 60000 },
+    { name: 'Huawei', price: 50000 },
+    { name: 'BMW', price: 90000 }
+  ];
+
   return (
     <div className="App">
-      <Product name="Samsung"></Product>
+      {
+        products.map(product =><Product name={product.name} price={product.price}></Product>)
+      }
+      {/* <Product name="Samsung" price="30000"></Product>
+      <Product name="Apple" price="60000"></Product>
+      <Product name="Huawei" price="40000"></Product> */}
     </div>
   );
 }
@@ -13,8 +25,8 @@ function App() {
 function Product(props) {
   return (
     <div className='product'>
-      <h3>Name:{props.name} </h3>
-      <p>Price: </p>
+      <h3>Name: {props.name} </h3>
+      <p>Price: {props.price} </p>
     </div>
   )
 }
