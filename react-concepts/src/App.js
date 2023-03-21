cdbdd7bfb2c9876b9ff2b84992152c7a639280d0
rakib8680 <rakib.cst.1st@gmail.com>
2023-03-21 19:01:23 +0600

    product

--- react-concepts/src/App.js
+++ react-concepts/src/App.js
@@ -4,22 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Product name="Samsung"></Product>
     </div>
   );
+}
+
+
+function Product(props) {
+  return (
+    <div className='product'>
+      <h3>Name:{props.name} </h3>
+      <p>Price: </p>
+    </div>
+  )
 }
 
 export default App;
