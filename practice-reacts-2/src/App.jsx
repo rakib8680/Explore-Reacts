3d885b964358b22e3f84f6c05e1380fa03f39570
rakib8680 <rakib.cst.1st@gmail.com>
2023-03-24 17:27:29 +0600

    css added

--- practice-reacts-2/src/App.jsx
+++ practice-reacts-2/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const style ={
+  fontSize : '30px',
+  fontWeight : 600
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <article style={style} className='blog'>Hello, World</article>
     </div>
   )
 }
